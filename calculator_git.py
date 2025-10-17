80124e0cd184a62ab3b1767e333a0d02dd1f10ee
Glabrata <69863476+Glabrata@users.noreply.github.com>
2025-10-17 14:43:09 +0200

    Update calculator_git.py

--- calculator_git.py
+++ calculator_git.py
@@ -218,10 +218,11 @@ summary = pd.DataFrame({
         total_cost_pv['total_hourly_cost'].sum() - 0.0028 * yearly_consumption
     ],
     'Savings (DKK)': [
-        total_cost_without_pv['spot_cost'].sum() - total_cost_pv['spot_cost'].sum(),
-        total_cost_without_pv['tariffs_cost'].sum() - total_cost_pv['tariff_cost'].sum(),
-        total_cost_without_pv['tax_cost'].sum() - total_cost_pv['energy_tax'].sum(),
-        total_cost_without_pv['total_hourly_cost'].sum() - total_cost_pv['total_hourly_cost'].sum()
+        total_cost_without_pv['spot_cost'].sum() - total_cost_pv['spot_cost'].sum() - 0.0012 * yearly_consumption,
+        total_cost_without_pv['tariffs_cost'].sum() - total_cost_pv['tariff_cost'].sum() - 0.0002 * yearly_consumption,
+        total_cost_without_pv['tax_cost'].sum() - total_cost_pv['energy_tax'].sum() - 0.0014 * yearly_consumption,
+        total_cost_without_pv['total_hourly_cost'].sum() - total_cost_pv['total_hourly_cost'].sum() - 0.0028 * yearly_consumption
     ]
 }, index=['Spot price cost', 'Tariff cost', 'Energy tax cost', 'Total'])
+
 
